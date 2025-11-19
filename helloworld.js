f76f80131b99cc27f5ba6a74a82d14615a2895e8
prameela <mekalaprameela11@gmail.com>
2025-11-19 09:42:38 +0530

    adding heelofile

---
+++ helloworld.js
@@ -0,0 +1,15 @@
+
+console.log("Hello, World!");
+console.log("i will smile 😀");//window+; for smile short cut
+let bmiform = document.querySelector(".dmi-form");
+bmiform.addEventListener("submit",function(e){
+    e.preventDefault();
+    const form =e.target;
+    let name =form.querySelector("#name").value;
+    let weight = parseFloat(form.querySelector("#weight").value);
+    let height = parseFloat(form.querySelector("#height").value);
+    let bmi = weight/(height*height);
+    console.log(weight,height,bmi);
+    form.querySelector("#result").textContent = `${name}, You are ${bmi.toFixed(2)}`;
+
+});
